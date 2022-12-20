c5174b7691b2534fcf16a8390d2d0e982ec1a64b
slowot <slowot.com@gmail.com>
2022-12-20 18:51:48 +0800

    [medium][298]: Length of String

---
+++ 00298-medium-length-of-string.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<LengthOfString<"">, 0>>,
+  Expect<Equal<LengthOfString<"kumiko">, 6>>,
+  Expect<Equal<LengthOfString<"reina">, 5>>,
+  Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+];
+
+// ============= Your Code Here =============
+type test = LengthOfString<"kumiko">;
+type StringToArray<S extends string> =
+  S extends `${infer ElementType}${infer Rest}`
+    ? [ElementType, ...StringToArray<Rest>]
+    : [];
+type LengthOfString<S extends string> = StringToArray<S>["length"];
