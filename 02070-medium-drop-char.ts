68913350447afae413e5e9c612abae9a07edf3c4
slowot <slowot.com@gmail.com>
2023-01-02 13:56:57 +0800

    [medium][2070]: Drop Char(Update the solution

--- 02070-medium-drop-char.ts
+++ 02070-medium-drop-char.ts
@@ -15,10 +15,7 @@ type cases = [
 // ============= Your Code Here =============
 type DropChar<
   S extends string,
-  C extends string,
-  Result extends string = ""
-> = S extends `${infer First}${infer Rest}`
-  ? First extends C
-    ? DropChar<Rest, C, Result>
-    : DropChar<Rest, C, `${Result}${First}`>
-  : Result;
+  C extends string
+> = S extends `${infer First}${C}${infer Rest}`
+  ? DropChar<`${First}${Rest}`, C>
+  : S;
