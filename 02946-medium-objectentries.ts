6230f5cd0126ad2d01b442d8a8d9b009f315993c
slowot <slowot.com@gmail.com>
2023-01-04 17:55:03 +0800

    [medium][2946]: ObjectEntries

---
+++ 02946-medium-objectentries.ts
@@ -0,0 +1,36 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries =
+  | ["name", string]
+  | ["age", number]
+  | ["locations", string[] | null];
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ["key", undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ["key", undefined]>>
+];
+
+// ============= Your Code Here =============
+type RemoveTypeInUnion<U, T> = U extends U
+  ? Equal<U, T> extends true
+    ? never
+    : U
+  : "Error";
+
+type ObjectEntries<T extends object> = {
+  [K in keyof T]-?: [
+    K,
+    RemoveTypeInUnion<T[K], undefined> extends never
+      ? T[K]
+      : RemoveTypeInUnion<T[K], undefined>
+  ];
+}[keyof T];
