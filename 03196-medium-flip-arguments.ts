b52e44a58a11bc44d3d050493adb8123adbb440d
slowot <slowot.com@gmail.com>
2023-01-05 12:07:37 +0800

    [medium][3196]: Flip Arguments

---
+++ 03196-medium-flip-arguments.ts
@@ -0,0 +1,37 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<FlipArguments<() => boolean>, () => boolean>>,
+  Expect<
+    Equal<FlipArguments<(foo: string) => number>, (foo: string) => number>
+  >,
+  Expect<
+    Equal<
+      FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>,
+      (arg0: boolean, arg1: number, arg2: string) => void
+    >
+  >
+];
+
+type errors = [
+  // @ts-expect-error
+  FlipArguments<"string">,
+  // @ts-expect-error
+  FlipArguments<{ key: "value" }>,
+  // @ts-expect-error
+  FlipArguments<["apple", "banana", 100, { a: 1 }]>,
+  // @ts-expect-error
+  FlipArguments<null | undefined>
+];
+
+// ============= Your Code Here =============
+type Reverse<T extends Array<unknown>> = T extends [infer First, ...infer Rest]
+  ? [...Reverse<Rest>, First]
+  : T;
+
+type FlipArguments<T extends (...args: Array<any>) => unknown> = T extends (
+  ...args: infer P
+) => infer R
+  ? (...args: Reverse<P>) => R
+  : "[FlipArguments]: Error";
