93918cf12a8e1fa9a5e86b57e43f0c813b215875
slowot <slowot.com@gmail.com>
2023-01-19 20:41:58 +0800

    [medium][5317]: LastIndexOf

---
+++ 05317-medium-lastindexof.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<LastIndexOf<[1, 2, 3, 2, 1], 2>, 3>>,
+  Expect<Equal<LastIndexOf<[2, 6, 3, 8, 4, 1, 7, 3, 9], 3>, 7>>,
+  Expect<Equal<LastIndexOf<[0, 0, 0], 2>, -1>>,
+  Expect<Equal<LastIndexOf<[string, 2, number, "a", number, 1], number>, 4>>,
+  Expect<Equal<LastIndexOf<[string, any, 1, number, "a", any, 1], any>, 5>>
+];
+
+// ============= Your Code Here =============
+type LastIndexOf<
+  A extends Array<unknown>,
+  Element,
+  Temp extends Array<unknown> = [],
+  Result extends number = -1
+> = A extends [infer First, ...infer Rest]
+  ? Equal<First, Element> extends true
+    ? LastIndexOf<Rest, Element, [...Temp, First], Temp["length"]>
+    : LastIndexOf<Rest, Element, [...Temp, First], Result>
+  : Result;
