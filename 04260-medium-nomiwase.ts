2173bdd8de913ff7d0a14444cf7ecaa980a8b9fc
slowot <slowot.com@gmail.com>
2023-01-19 16:15:29 +0800

    [medium][4260]: AllCombinations

---
+++ 04260-medium-nomiwase.ts
@@ -0,0 +1,120 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<AllCombinations<"">, "">>,
+  Expect<Equal<AllCombinations<"A">, "" | "A">>,
+  Expect<Equal<AllCombinations<"AB">, "" | "A" | "B" | "AB" | "BA">>,
+  Expect<
+    Equal<
+      AllCombinations<"ABC">,
+      | ""
+      | "A"
+      | "B"
+      | "C"
+      | "AB"
+      | "AC"
+      | "BA"
+      | "BC"
+      | "CA"
+      | "CB"
+      | "ABC"
+      | "ACB"
+      | "BAC"
+      | "BCA"
+      | "CAB"
+      | "CBA"
+    >
+  >,
+  Expect<
+    Equal<
+      AllCombinations<"ABCD">,
+      | ""
+      | "A"
+      | "B"
+      | "C"
+      | "D"
+      | "AB"
+      | "AC"
+      | "AD"
+      | "BA"
+      | "BC"
+      | "BD"
+      | "CA"
+      | "CB"
+      | "CD"
+      | "DA"
+      | "DB"
+      | "DC"
+      | "ABC"
+      | "ABD"
+      | "ACB"
+      | "ACD"
+      | "ADB"
+      | "ADC"
+      | "BAC"
+      | "BAD"
+      | "BCA"
+      | "BCD"
+      | "BDA"
+      | "BDC"
+      | "CAB"
+      | "CAD"
+      | "CBA"
+      | "CBD"
+      | "CDA"
+      | "CDB"
+      | "DAB"
+      | "DAC"
+      | "DBA"
+      | "DBC"
+      | "DCA"
+      | "DCB"
+      | "ABCD"
+      | "ABDC"
+      | "ACBD"
+      | "ACDB"
+      | "ADBC"
+      | "ADCB"
+      | "BACD"
+      | "BADC"
+      | "BCAD"
+      | "BCDA"
+      | "BDAC"
+      | "BDCA"
+      | "CABD"
+      | "CADB"
+      | "CBAD"
+      | "CBDA"
+      | "CDAB"
+      | "CDBA"
+      | "DABC"
+      | "DACB"
+      | "DBAC"
+      | "DBCA"
+      | "DCAB"
+      | "DCBA"
+    >
+  >
+];
+
+// ============= Your Code Here =============
+type StringToUnion<S extends string> = S extends `${infer First}${infer Rest}`
+  ? First | StringToUnion<Rest>
+  : "";
+
+type Contain<
+  Left extends string,
+  Right extends string
+> = Left extends `${infer _}${Right}${infer _}` ? true : false;
+
+type AllCombinations<
+  S extends string,
+  U extends string = "",
+  Iterator extends string = StringToUnion<S>,
+  Result extends string = Iterator extends Iterator
+    ? U extends U
+      ? `${Iterator}${Contain<U, Iterator> extends true ? never : U}`
+      : never
+    : never
+> = S extends Result ? Result : Result | AllCombinations<S, Result>;
