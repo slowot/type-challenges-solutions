002cc636904e9a0ef5ea8b8e9b0f6a8d9dbf976f
slowot <slowot.com@gmail.com>
2023-01-19 19:21:43 +0800

    [medium][4518]: Fill

---
+++ 04518-medium-fill.ts
@@ -0,0 +1,32 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+import { PlusOne } from "./utils";
+
+type cases = [
+  Expect<Equal<Fill<[], 0>, []>>,
+  Expect<Equal<Fill<[], 0, 0, 3>, []>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 0, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0, 2, 2>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], 0>, [0, 0, 0]>>,
+  Expect<Equal<Fill<[1, 2, 3], true>, [true, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 1>, [true, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 1, 3>, [1, true, true]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 0>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 10, 20>, [1, 2, 3]>>,
+  Expect<Equal<Fill<[1, 2, 3], true, 0, 10>, [true, true, true]>>
+];
+
+// ============= Your Code Here =============
+type Fill<
+  A extends Array<unknown>,
+  Element,
+  Start extends number = 0,
+  End extends number = A["length"],
+  Result extends Array<unknown> = []
+> = Start extends End
+  ? [...Result, ...A]
+  : A extends [infer First, ...infer Rest]
+  ? Result["length"] extends Start
+    ? Fill<Rest, Element, PlusOne<Start>, End, [...Result, Element]>
+    : Fill<Rest, Element, Start, End, [...Result, First]>
+  : Result;
